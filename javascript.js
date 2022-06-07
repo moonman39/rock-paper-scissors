022d52fd8ceb48dbc01510a1b5dd95f33c03e602
Grant Luna <grant.luna@fnf.com>
2022-06-06 21:31:13 -0700

    Create singleRound funciton to simulate a single round of the rps game.

--- javascript.js
+++ javascript.js
@@ -12,6 +12,31 @@ const computerPlay = () => {
   }
 };
 
+let playerSelection = "ROCK";
+let computerSelection = computerPlay();
+
+const singleRound = (playerSelection, computerSelection) => {
+  let player = playerSelection.toLowerCase();
+  let computer = computerSelection.toLowerCase();
+  if (player === "rock" && computer === "paper") {
+    return `You lose! Paper beats rock.`;
+  } else if (player === "rock" && computer === "scissors") {
+    return `You win! Rock beats scissors.`;
+  } else if (player === "paper" && computer === "scissors") {
+    return `You lose! Scissors beats paper.`;
+  } else if (player === "paper" && computer === "rock") {
+    return `You win! Paper beats rock.`;
+  } else if (player === "scissors" && computer === "rock") {
+    return `You lose! Rock beats scissors.`;
+  } else if (player === "scissors" && computer === "paper") {
+    return `You win! Scissors beats paper.`;
+  } else {
+    return `You tied! You both had ${player}`;
+  }
+};
+
+console.log(singleRound(playerSelection, computerSelection));
+
 //This is a magic eight ball game
 
 /* 
