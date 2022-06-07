8402f2c17fcffb9faa1e53af1daa4ff7956f4c0e
Grant Luna <grant.luna@fnf.com>
2022-06-07 07:23:49 -0700

    Add DOM scripts to RPS game.  Reworked singleRound function.  Delete game funciton.

--- javascript.js
+++ javascript.js
@@ -1,5 +1,21 @@
 // This is a rock, paper, scissors game against a computer
 
+// Querying the DOM
+const rock = document.querySelector(".rock");
+const paper = document.querySelector(".paper");
+const scissors = document.querySelector(".scissors");
+const results = document.querySelector(".result-content");
+const game = document.querySelector(".game");
+const newGame = document.querySelector(".new-game");
+
+// Work on updating the score
+let computerScore = 0;
+let userScore = 0;
+const user = document.querySelector(".user");
+const computer = document.querySelector(".computer");
+user.textContent = userScore;
+computer.textContent = computerScore;
+
 // This is a function to generate a computer guess
 const computerPlay = () => {
   let computerGuess = Math.floor(Math.random() * 3);
@@ -12,65 +28,118 @@ const computerPlay = () => {
   }
 };
 
-let computerScore = 0;
-let userScore = 0;
-
+// A single round function that updates the HTML with the round results
 const singleRound = (playerSelection, computerSelection) => {
   let player = playerSelection.toLowerCase();
   let computer = computerSelection.toLowerCase();
   if (player === "rock" && computer === "paper") {
+    results.textContent = `You chose rock.  The computer chose paper.  You lost!`;
     computerScore++;
-    return `You lose! Paper beats rock.  You score: ${userScore} | Computer score: ${computerScore}`;
   } else if (player === "rock" && computer === "scissors") {
+    results.textContent = `You chose rock.  The computer chose scissors.  You won!`;
     userScore++;
-    return `You win! Rock beats scissors.  You score: ${userScore} | Computer score: ${computerScore}`;
   } else if (player === "paper" && computer === "scissors") {
+    results.textContent = `You chose paper.  The computer chose scissors.  You lost!`;
     computerScore++;
-    return `You lose! Scissors beats paper.  You score: ${userScore} | Computer score: ${computerScore}`;
   } else if (player === "paper" && computer === "rock") {
+    results.textContent = `You chose paper.  The computer chose rock.  You won!`;
     userScore++;
-    return `You win! Paper beats rock.  You score: ${userScore} | Computer score: ${computerScore}`;
   } else if (player === "scissors" && computer === "rock") {
+    results.textContent = `You chose scissors.  The computer chose rock.  You lost!`;
     computerScore++;
-    return `You lose! Rock beats scissors.  You score: ${userScore} | Computer score: ${computerScore}`;
   } else if (player === "scissors" && computer === "paper") {
+    results.textContent = `You chose scissors.  The computer chose paper.  You won!`;
     userScore++;
-    return `You win! Scissors beats paper.  You score: ${userScore} | Computer score: ${computerScore}`;
   } else {
-    return `You tied! You both had ${player}.  You score: ${userScore} | Computer score: ${computerScore}`;
+    results.textContent = `You both chose ${computer}.  You tied!`;
+  }
+
+  //   work on a function that tracks the first to 5 and starts the game over
+  if (userScore === 3) {
+    game.classList.add("hidden");
+    newGame.classList.remove("hidden");
+    results.textContent = `You won! Select "New Game" to play again.`;
+  } else if (computerScore === 3) {
+    game.classList.add("hidden");
+    newGame.classList.remove("hidden");
+    results.textContent = `You lost! Select "New Game" to play again.`;
   }
 };
 
-const game = () => {
-  for (let i = 0; i < 100; i++) {
-    // get a value for playerSelection and verify they put in a value
-    let playerSelection = prompt("Do you choose rock, paper, or scissors?");
-    while (!playerSelection) {
-      playerSelection = prompt("Do you choose rock, paper, or scissors?");
-    }
+// Rock Button Event Listener
+rock.addEventListener("click", () => {
+  let playerSelection = "rock";
+  let computerSelection = computerPlay();
+  singleRound(playerSelection, computerSelection);
+  user.textContent = userScore;
+  computer.textContent = computerScore;
+});
 
-    // get a value for computerSelection by calling computerPlay()
-    let computerSelection = computerPlay();
-
-    // play a single round of rock, paper, scissors using the above values
-    console.log(singleRound(playerSelection, computerSelection));
-
-    // track the first player to 3 wins, or a tie
-    if (userScore === 3) {
-      console.log(
-        `You won! You had ${userScore} points while the computer had ${computerScore} points.`
-      );
-      break;
-    } else if (computerScore === 3) {
-      console.log(
-        `You lost! The computer had ${computerScore} points while you had ${userScore} points.`
-      );
-      break;
-    }
+// Paper Button Event Listener
+paper.addEventListener("click", () => {
+  if (userScore === 3 || computerScore === 3) {
+    user.textContent = 0;
+    computer.textContent = 0;
   }
-};
+  let playerSelection = "scissors";
+  let computerSelection = computerPlay();
+  singleRound(playerSelection, computerSelection);
+  user.textContent = userScore;
+  computer.textContent = computerScore;
+});
 
-console.log(game());
+// Scissors Button Event Listener
+scissors.addEventListener("click", () => {
+  let playerSelection = "scissors";
+  let computerSelection = computerPlay();
+  singleRound(playerSelection, computerSelection);
+  user.textContent = userScore;
+  computer.textContent = computerScore;
+});
+
+// New Game Button Event Listener
+newGame.addEventListener("click", () => {
+  userScore = 0;
+  computerScore = 0;
+  user.textContent = userScore;
+  computer.textContent = computerScore;
+  game.classList.remove("hidden");
+  newGame.classList.add("hidden");
+  results.textContent = ``;
+});
+
+// const game = () => {
+//   for (let i = 0; i < 100; i++) {
+//     // get a value for playerSelection and verify they put in a value
+//     let playerSelection = prompt("Do you choose rock, paper, or scissors?");
+//     while (!playerSelection) {
+//       playerSelection = prompt("Do you choose rock, paper, or scissors?");
+//     }
+
+//     // get a value for computerSelection by calling computerPlay()
+//     let computerSelection = computerPlay();
+
+//     // play a single round of rock, paper, scissors using the above values
+//     console.log(singleRound(playerSelection, computerSelection));
+
+//     // track the first player to 3 wins, or a tie
+//     if (userScore === 3) {
+//       console.log(
+//         `You won! You had ${userScore} points while the computer had ${computerScore} points.`
+//       );
+//       break;
+//     } else if (computerScore === 3) {
+//       console.log(
+//         `You lost! The computer had ${computerScore} points while you had ${userScore} points.`
+//       );
+//       break;
+//     } else if (i === 20) {
+//       console.log(
+//         `Ok game over! The audience is bored of the ties.  The score is tied at ${userScore} points.`
+//       );
+//     }
+//   }
+// };
 
 //This is a magic eight ball game
 
