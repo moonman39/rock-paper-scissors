ac432e2e66844416a9122b69442b5cefb93c7d93
Grant Luna <grant.luna@fnf.com>
2022-06-06 21:51:44 -0700

    Create game function to see the first player to 3 wins.

--- javascript.js
+++ javascript.js
@@ -12,30 +12,65 @@ const computerPlay = () => {
   }
 };
 
-let playerSelection = "ROCK";
-let computerSelection = computerPlay();
+let computerScore = 0;
+let userScore = 0;
 
 const singleRound = (playerSelection, computerSelection) => {
   let player = playerSelection.toLowerCase();
   let computer = computerSelection.toLowerCase();
   if (player === "rock" && computer === "paper") {
-    return `You lose! Paper beats rock.`;
+    computerScore++;
+    return `You lose! Paper beats rock.  You score: ${userScore} | Computer score: ${computerScore}`;
   } else if (player === "rock" && computer === "scissors") {
-    return `You win! Rock beats scissors.`;
+    userScore++;
+    return `You win! Rock beats scissors.  You score: ${userScore} | Computer score: ${computerScore}`;
   } else if (player === "paper" && computer === "scissors") {
-    return `You lose! Scissors beats paper.`;
+    computerScore++;
+    return `You lose! Scissors beats paper.  You score: ${userScore} | Computer score: ${computerScore}`;
   } else if (player === "paper" && computer === "rock") {
-    return `You win! Paper beats rock.`;
+    userScore++;
+    return `You win! Paper beats rock.  You score: ${userScore} | Computer score: ${computerScore}`;
   } else if (player === "scissors" && computer === "rock") {
-    return `You lose! Rock beats scissors.`;
+    computerScore++;
+    return `You lose! Rock beats scissors.  You score: ${userScore} | Computer score: ${computerScore}`;
   } else if (player === "scissors" && computer === "paper") {
-    return `You win! Scissors beats paper.`;
+    userScore++;
+    return `You win! Scissors beats paper.  You score: ${userScore} | Computer score: ${computerScore}`;
   } else {
-    return `You tied! You both had ${player}`;
+    return `You tied! You both had ${player}.  You score: ${userScore} | Computer score: ${computerScore}`;
   }
 };
 
-console.log(singleRound(playerSelection, computerSelection));
+const game = () => {
+  for (let i = 0; i < 100; i++) {
+    // get a value for playerSelection and verify they put in a value
+    let playerSelection = prompt("Do you choose rock, paper, or scissors?");
+    while (!playerSelection) {
+      playerSelection = prompt("Do you choose rock, paper, or scissors?");
+    }
+
+    // get a value for computerSelection by calling computerPlay()
+    let computerSelection = computerPlay();
+
+    // play a single round of rock, paper, scissors using the above values
+    console.log(singleRound(playerSelection, computerSelection));
+
+    // track the first player to 3 wins, or a tie
+    if (userScore === 3) {
+      console.log(
+        `You won! You had ${userScore} points while the computer had ${computerScore} points.`
+      );
+      break;
+    } else if (computerScore === 3) {
+      console.log(
+        `You lost! The computer had ${computerScore} points while you had ${userScore} points.`
+      );
+      break;
+    }
+  }
+};
+
+console.log(game());
 
 //This is a magic eight ball game
 
